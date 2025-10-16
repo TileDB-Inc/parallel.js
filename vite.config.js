4d36278cbcc5ff7bd257bacb22f3f06c97d65977
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2025-10-17 02:59:05 +0300

    Make webworker webpack friendly

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,31 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    base: '',
-    build: {
-        outDir: 'lib',
-        assetsDir: '',
-        emptyOutDir: true,
-        lib: {
-            entry: 'src/index.ts',
-            formats: ['es'],
-            fileName: 'index'
-        },
-        minify: true
+  base: '',
+  build: {
+    outDir: 'lib',
+    assetsDir: '',
+    emptyOutDir: true,
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: 'index'
     },
-    worker: {
-        format: "es",
-    },
-    plugins: [dts({ rollupTypes: true })]
+    minify: true
+  },
+  worker: {
+    format: "es",
+  },
+  plugins: [
+    dts({ rollupTypes: true }),
+    {
+      name: "webpack5ify-webworker",
+      renderChunk: {
+        order: 'post',
+        async handler(code) {
+          return code.replaceAll('.href', '');
+        }
+      }
+    }
+  ]
 })
